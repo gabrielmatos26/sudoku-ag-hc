58c156511fd824434da68b6dbf8ad22698aa2376
gabrielmatos26 <matos.gabriel26@gmail.com>
2017-06-18 21:40:17 -0300

    adicionadas funcoes pra mutacao e crossover

--- ag.py
+++ ag.py
@@ -1,4 +1,4 @@
-import numpy as np; np.random.seed(1234)
+import numpy as np;
 from numpy.random import random
 import math
 
@@ -55,7 +55,46 @@ def printBoard(sol_string):
         print(a[i,3:6], a[i+1,3:6], a[i+2,3:6])
         print(a[i,6:], a[i+1,6:], a[i+2,6:])
 
+#select a portion of initial population based on fitness function
+def matingPool(population, ranks):
+    quantity = len(population)
+    weights = np.asarray([np.sum(ranks)] * ranks.size) - ranks
+    weights = weights.astype('float32') / np.sum(weights)
+    indexes = np.random.choice(np.arange(len(population)), size=quantity, p=weights)
+    return population[indexes]
+
+#check if crossover happens, then calculate crossover point
+def crossover(sol_string1, sol_string2, crossover_prob):
+    if np.random.random() < crossover_prob:
+        cross_point = np.random.randint(9) * 9
+        new_sol_string1 = np.concatenate((sol_string1[:cross_point], sol_string2[cross_point:]))
+        new_sol_string2 = np.concatenate((sol_string2[:cross_point], sol_string1[cross_point:]))
+        return new_sol_string1, new_sol_string2
+    return sol_string1, sol_string2
+
+def mutation(initial_grid, sol_string, mutation_prob):
+    m_sol_string = sol_string.copy()
+    i = np.random.randint(9)
+    if np.random.random() < mutation_prob:
+        s1,s2 = tuple(np.random.randint(9, size=2))
+        while initial_grid[s1*i] != 0 or initial_grid[s2*i] != 0:
+            s1,s2 = tuple(np.random.randint(9, size=2))
+        m_sol_string[s1*i], m_sol_string[s2*i] = m_sol_string[s2*i], m_sol_string[s1*i]
+    return m_sol_string
+
+def rankPopulation(population):
+    fitness_table = []
+    for sol in population:
+        fitness_table.append(fitnessFunction(sol))
+    return np.asarray(fitness_table)
+
+
 def main():
+    NGENS = 100
+    POPSIZE = 1000
+    MUTATION_RATE = 0.1
+    CROSSOVER_RATE = 0.85
+
     initial_grid = np.array([0,0,0,1,0,0,0,0,0,
                              7,0,0,0,0,0,4,3,0,
                              0,0,0,0,0,0,2,0,0,
@@ -66,11 +105,39 @@ def main():
                              0,8,1,0,0,0,0,0,0,
                              0,0,0,0,5,0,3,0,0])
 
-    initial_pop = generateInitialPopulation(initial_grid, 9, 30)
-    fitness_table = []
-    for sol in initial_pop:
-        fitness_table.append(fitnessFunction(sol))
-    print(fitness_table)
+    population = generateInitialPopulation(initial_grid, 9, POPSIZE)
+    best_population = None
+    local_minima = 0
+    print('Initial configuration:')
+    printBoard(initial_grid)
+    previous_score = np.inf
+    for gen in range(NGENS):
+        #rank population by fitness function
+        fitness_table = rankPopulation(population)
+        best_population = population.copy()
+        best_result, worst_result = np.min(fitness_table), np.max(fitness_table)
+        if (previous_score - best_result) == 0:
+            local_minima += 1
+        if best_result == 0 or local_minima >= 10:
+            print("Generation: %d: best score: %d, worst score: %d" % (gen, best_result, worst_result))
+            print('\n\nBest Solution:')
+            printBoard(population[p.argmin(fitness_table)])
+            break
+        print("Generation: %d: best score: %d, worst score: %d" % (gen, best_result, worst_result))
+
+        #apply mutation and crossover to generate next gen population
+        pairs = np.random.permutation(np.arange(POPSIZE)).reshape(np.int(POPSIZE/2), 2)
+        new_pop = population.copy()
+        for pair in pairs:
+            p1,p2 = tuple(pair)
+            new_pop[p1], new_pop[p2] = crossover(new_pop[p1], new_pop[p2], MUTATION_RATE)
+        for i, sol in enumerate(new_pop):
+            new_pop[i] = mutation(initial_grid, sol, MUTATION_RATE)
+        population = matingPool(new_pop.copy(), np.array([fitnessFunction(sol) for sol in new_pop]))
+
+    print('\n\nBest Solution:')
+    printBoard(best_population[np.argmin(fitness_table)])
+
 
 if __name__ == '__main__':
     main()
